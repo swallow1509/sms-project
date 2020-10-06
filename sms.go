c2680f05ea01a1f6fefe077af07e3823ba61f079
swallow1509 <umidbek.yusupov.98@mail.ru>
2020-10-07 01:05:52 +0500

    Working prototype with Twilio(unfortunately)

--- sms.go
+++ sms.go
@@ -1,38 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
-//Const values as initial varibles
 const (
 	accountSID = "AC8a33c89d0a3b3f53d455c55e720f4805"
-	authToken  = "5edae12bc1a68a01d5050fdd50dc0c37"
-	uslString  = "https://api.twilio.com/2010-04-01/Accounts/" + accountSID + "/Messages.json"
+	authToken  = "b57f7e3e1678236762b7bdca5fe22d90"
+	urlString  = "https://api.twilio.com/2010-04-01/Accounts/AC8a33c89d0a3b3f53d455c55e720f4805/Messages.json"
 )
 
 func main() {
 
-	//Basic data for message
-	val := url.Values{}
-	val.Set("To", "+998990777558")
-	val.Set("From", "+16266289463")
-	val.Set("Body", "Sardorbek qaleysiz? Men Umidjonman. Amerikadan SMS jonatyapman")
-	reqBody := *strings.NewReader(val.Encode())
+	// Basic data for our message
+	v := url.Values{}
+	v.Set("To", "+998998280098")
+	v.Set("From", "+16266289463")
+	v.Set("Body", "hehehe Umidjon qaleysiz?")
+	reqBody := *strings.NewReader(v.Encode())
 
-	//New client
+	// New client
 	client := &http.Client{}
 
-	req, _ := http.NewRequest("POST", uslString, &reqBody)
+	req, _ := http.NewRequest("POST", urlString, &reqBody)
 	req.SetBasicAuth(accountSID, authToken)
 	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	//Create request
+	// Make request
 	resp, _ := client.Do(req)
-	fmt.Println(resp.Status)
-
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		var data map[string]interface{}
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		err := json.Unmarshal(bodyBytes, &data)
+		if err == nil {
+			fmt.Println(data["sid"])
+		}
+	} else {
+		fmt.Println(resp.Status)
+	}
 }
